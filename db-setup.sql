0f2d8478c4005ab77b0b03088c82db97a36b8c54
mikedilulo <mikedilulo2019@gmail.com>
2020-02-28 15:35:37 -0700

    Created / Select / Drop / Truncate Keeps Table

--- db-setup.sql
+++ db-setup.sql
@@ -1,1 +1,23 @@
+USE keeper007;
 
+-- CREATE TABLE keeps(
+--     id int AUTO_INCREMENT NOT NULL,
+--     name VARCHAR(100) NOT NULL,
+--     description VARCHAR(255) NOT NULL,
+--     img VARCHAR(255) NOT NULL,
+--     createdBy VARCHAR(100) NOT NULL,
+--     timeStamp DATETIME NOT NULL,
+--     userId VARCHAR(255),
+--     isPrivate TINYINT,
+--     views INT DEFAULT 0,
+--     shares INT DEFAULT 0,
+--     keeps INT DEFAULT 0,
+--     INDEX userId(userId),
+--     PRIMARY KEY (id)
+-- );
+
+-- SELECT * FROM keeps;
+
+-- DROP TABLE keeps;
+
+-- TRUNCATE TABLE keeps;
