d97ed6c49697a07ef10bc23b9724ba1a5073cde7
mikedilulo <mikedilulo2019@gmail.com>
2020-02-29 17:18:31 -0700

    Took off Keeps and Vaults pages.

--- wwwsrc/src/components/ContactNavbar.vue
+++ wwwsrc/src/components/ContactNavbar.vue
@@ -23,12 +23,6 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active mr-4">
-                <a class="nav-link" href="#">Keeps</a>
-              </li>
-              <li class="nav-item active mr-4">
-                <a class="nav-link" href="#">Vaults</a>
-              </li>
-              <li class="nav-item active mr-4">
                 <a class="nav-link" href="#">Users</a>
               </li>
               <li class="nav-item active mr-4">
