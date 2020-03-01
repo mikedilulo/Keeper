1162e421ff96123f88ef51743e09abe63452eb25
mikedilulo <mikedilulo2019@gmail.com>
2020-03-01 13:37:43 -0700

    Added Detail Views for Public and Private Keeps

---
+++ wwwsrc/src/views/PrivateKeepDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="privatekeepdetails"></div>
+</template>
+
+<script>
+export default {
+  name: "privatekeepdetails"
+};
+</script>
+
+<style>
+</style>
---
+++ wwwsrc/src/views/PublicKeepDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="publickeepdetails"></div>
+</template>
+
+<script>
+export default {
+  name: "publickeepdetails"
+};
+</script>
+
+<style>
+</style>
