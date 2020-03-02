3ee9f3d4545f383e3f561ae7eeda6a7a307476db
mikedilulo <mikedilulo2019@gmail.com>
2020-03-01 17:53:32 -0700

    Public Keep Details Page Complete minus style

--- wwwsrc/src/views/PublicKeepDetails.vue
+++ wwwsrc/src/views/PublicKeepDetails.vue
@@ -1,12 +1,63 @@
 <template>
-  <div class="publickeepdetails"></div>
+  <div class="publickeepdetails container-fluid pr-0 pl-0">
+    <aboutnav />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-5 mt-5">
+          <img :src="activePublicKeep.img" class="latest-card image" alt />
+        </div>
+        <div class="col-md-5 mt-5">
+          <h5 class="publickeepinfo ml-5 mb-4">
+            Keep Created By:
+            <span class="text-danger">{{activePublicKeep.createdBy}}</span>
+          </h5>
+          <h5 class="ml-5 mb-4">
+            Keep Name:
+            <span class="text-danger">{{activePublicKeep.name}}</span>
+          </h5>
+          <h5 class="ml-5">
+            Keep Description:
+            <span class="text-danger">{{activePublicKeep.description}}</span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import aboutnav from "@/components/AboutNavbar.vue";
 export default {
-  name: "publickeepdetails"
+  name: "publickeepdetails",
+  mounted() {
+    this.$store.dispatch("getPublicKeepById", this.$route.params.id);
+  },
+  props: ["keepData"],
+  computed: {
+    activePublicKeep() {
+      return this.$store.state.activePublicKeep;
+    }
+  },
+  components: {
+    aboutnav
+  }
 };
 </script>
 
 <style>
+.latest-card {
+  border-radius: 10px;
+  max-width: 100%;
+  max-height: 100%;
+  position: relative;
+}
+.image {
+  z-index: -1;
+  max-width: 100%;
+  max-height: 100%;
+  position: relative;
+}
+.publickeepinfo {
+  margin-top: 75px;
+}
 </style>
