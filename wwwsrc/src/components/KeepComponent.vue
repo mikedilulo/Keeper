423abb283801ec510e573702211a4f0dbe680a26
mikedilulo <mikedilulo2019@gmail.com>
2020-03-02 12:57:04 -0700

    PublicKeep setup / can see details and delete

--- wwwsrc/src/components/KeepComponent.vue
+++ wwwsrc/src/components/KeepComponent.vue
@@ -3,129 +3,33 @@
     <div class="row">
       <div class="col-md-2 mb-5 mt-5">
         <div class="latest-card">
-          <div class="dropdown">
-            <button
-              class="btn btn-light dropdown-toggle d-flex justify-content-end ml-1"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            ></button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <div>
-                <button class="dropdown-item"></button>
-              </div>
-            </div>
-          </div>
           <div class="card-body p-0">
+            <button type="submit" class="btn btn-dark btn-sm button">
+              <i class="fas fa-chevron-circle-down"></i>
+            </button>
             <img :src="keepData.img" class="latest-card image" alt />
             <dd class="d-flex justify-content-between">
-              <button
-                class="btn btn-light btn-sm button"
-                type="button"
-                data-toggle="modal"
-                data-target="#shareModal"
-              >
-                <i class="fas fa-share-square"></i>
-              </button>
-              <button
-                class="btn btn-light btn-sm button"
-                type="button"
-                data-toggle="modal"
-                data-target="#informationModal"
-              >
-                <i class="fas fa-ellipsis-h"></i>
-              </button>
-            </dd>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Information Modal -->
-    <div
-      class="modal fade"
-      id="informationModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Keep Information</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <h4>
-              <i class="fab fa-korvue mr-1"></i>:
-              <span>0</span>
-            </h4>
-            <h4>
-              <i class="fas fa-share-square mr-1"></i>:
-              <span>0</span>
-            </h4>
-            <h4>
-              <i class="fas fa-eye mr-1"></i>:
-              <span>0</span>
-            </h4>
-            <div class="modal-footer d-flex justify-content-between">
-              <button
-                type="submit"
-                class="btn btn-warning"
-                data-dismiss="modal"
-                @click="deleteKeep(keepData.id)"
-              >Delete</button>
               <router-link :to="{name: 'publicKeepDetails', params: {id: keepData.id}}">
                 <button
                   type="button"
-                  class="btn btn-outline-dark"
+                  class="btn btn-dark btn-sm button"
                   @click="setActivePublicKeep(keepData.id)"
                   data-dismiss="modal"
-                >Details</button>
+                >
+                  <i class="fas fa-ellipsis-h"></i>
+                </button>
               </router-link>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Share Modal -->
-    <div
-      class="modal fade"
-      id="shareModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Share Your Keep</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form>
-              <div class="form-group">
-                <label for="recipient-name" class="col-form-label">Recipient:</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="recipient-name"
-                  placeholder="Type Email Here"
-                />
-              </div>
-              <button type="button" class="btn btn-success">Send Keep</button>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-dark btn-sm button">
+                <i class="fas fa-share-square"></i>
+              </button>
+              <button
+                type="submit"
+                class="btn btn-dark btn-sm button"
+                @click="deleteKeep(keepData.id)"
+              >
+                <i class="fas fa-trash"></i>
+              </button>
+            </dd>
           </div>
         </div>
       </div>
