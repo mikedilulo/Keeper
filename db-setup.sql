5411bd5edb2916601e32643b1ad90479350f4602
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 15:39:25 -0700

    Removed Pri in Modal / added Vault to setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -16,8 +16,23 @@ USE keeper007;
 --     PRIMARY KEY (id)
 -- );
 
+-- CREATE TABLE vaults(
+--     id int AUTO_INCREMENT NOT NULL,
+--     name VARCHAR(100) NOT NULL,
+--     description VARCHAR(255) NOT NULL,
+--     img VARCHAR(255) NOT NULL,
+--     createdBy VARCHAR(100) NOT NULL,
+--     timeStamp DATETIME,
+--     userId VARCHAR(255),
+--     INDEX userId(userId),
+--     PRIMARY KEY (id)
+-- );
+
 -- SELECT * FROM keeps;
+-- SELECT * FROM vaults;
 
 -- TRUNCATE TABLE keeps;
+-- TRUNCATE TABLE vaults;
 
 -- DROP TABLE keeps;
+-- DROP TABLE vaults;
