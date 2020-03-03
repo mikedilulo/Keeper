70c668770f2ae559a3d8dda864b2759eeadae21a
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 15:52:23 -0700

    Get methods completed for Vaults in Controller

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using Keepr.Models;
 using Keepr.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keepr.Controllers
@@ -9,6 +13,32 @@ namespace Keepr.Controllers
     public VaultsController(VaultsService vs)
     {
       _vs = vs;
+    }
+    [HttpGet("private")]
+    [Authorize]
+    public ActionResult<IEnumerable<Vault>> Get()
+    {
+      try
+      {
+        return Ok(_vs.GetVaultsByUserId);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpGet("private/{id}")]
+    [Authorize]
+    public ActionResult<Vault> GetById(int id)
+    {
+      try
+      {
+        return Ok(_vs.GetVaultById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
     }
   }
 }
