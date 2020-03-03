8c30ea03ee681b0d9651a9f82013e7bd60a50435
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 15:58:38 -0700

    Create / Edit methods completed in controller

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using Keepr.Models;
 using Keepr.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -34,6 +35,36 @@ namespace Keepr.Controllers
       try
       {
         return Ok(_vs.GetVaultById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpPost]
+    [Authorize]
+    public ActionResult<Vault> Create([FromBody] Vault newVault)
+    {
+      try
+      {
+        var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        newVault.UserId = userId;
+        return Ok(_vs.CreateVault(newVault));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpPut("private/{id}")]
+    [Authorize]
+    public ActionResult<Vault> Edit([FromBody] Vault newEditedVault, int id)
+    {
+      try
+      {
+        newEditedVault.Id = id;
+        newEditedVault.UserId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        return Ok(_vs.EditVaultById(newEditedVault));
       }
       catch (Exception e)
       {
