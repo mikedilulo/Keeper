71425b9fc70a18f57e1a36d311d202126b358853
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 15:36:19 -0700

    Vault Service setup prior to business logic

--- Services/VaultsService.cs
+++ Services/VaultsService.cs
@@ -1,7 +1,14 @@
+using Keepr.Repositories;
+
 namespace Keepr.Services
 {
   public class VaultsService
   {
+    private readonly VaultsRepository _vrepo;
+    public VaultsService(VaultsRepository vrepo)
+    {
+      _vrepo = vrepo;
+    }
 
   }
 }
