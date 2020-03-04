359a5e4fb9ebc1d54de9d4098d0d989b9ab560f5
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:36:09 -0700

    Frequently Asked Questions Privacy Coming Soon

---
+++ wwwsrc/src/views/QuestionsPrivacy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="questionsprivacy">
+    <h1>Frequently Asked Questions About Privacy coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "questionsprivacy"
+};
+</script>
+
+<style>
+</style>
