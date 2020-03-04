1cb6ff7ffca3e21ba9622891774cbffacccf772e
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:29:58 -0700

    Information Legal Page Coming Soon

---
+++ wwwsrc/src/views/InformationLegal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="informationlegal">
+    <h1>Information About Legal is coming soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "informationlegal"
+};
+</script>
+
+<style>
+</style>
