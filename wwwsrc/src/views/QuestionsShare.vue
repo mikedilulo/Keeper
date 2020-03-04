96e2926f3aed1d8e280d94597b7f2fd9ca53ea2e
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:34:20 -0700

    Frequently Asked Questions Share Coming Soon

---
+++ wwwsrc/src/views/QuestionsShare.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="questionsshare">
+    <h1>Frequently Asked Questions About Sharing coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "questionsshare"
+};
+</script>
+
+<style>
+</style>
