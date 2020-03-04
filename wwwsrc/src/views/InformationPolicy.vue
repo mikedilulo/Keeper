57126fdbcc79c45a2f6d2ae3dd6b707b54bc9e9f
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:30:39 -0700

    Information Policy Page Coming Soon

---
+++ wwwsrc/src/views/InformationPolicy.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="informationpolicy">
+    <h1>Information Policy Page Coming soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "informationpolicy"
+};
+</script>
+
+<style>
+</style>
