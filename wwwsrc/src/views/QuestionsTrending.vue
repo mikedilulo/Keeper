cd0b4cd01c3b6482002db838f694f95e899fe834
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:35:09 -0700

    Frequently Asked Questions Trending Coming Soon

---
+++ wwwsrc/src/views/QuestionsTrending.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="questionstrending">
+    <h1>Frequently Asked Questions About Trending Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "questionstrending"
+};
+</script>
+
+<style>
+</style>
