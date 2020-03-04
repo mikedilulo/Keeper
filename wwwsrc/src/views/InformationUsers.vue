657cb9f57f59a3140c3fcde3f0af1252ef829324
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:29:27 -0700

    Information User Page Coming Soon

---
+++ wwwsrc/src/views/InformationUsers.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="informationuser">
+    <h1>Information About Users Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "informationuser"
+};
+</script>
+
+<style>
+</style>
