8e43a98c54752ccd2ae980a4e87c3ef23ccf47e2
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:32:49 -0700

    Frequently Asked Questions About Vault coming soon

---
+++ wwwsrc/src/views/QuestionsVault.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="questionsvault">
+    <h1>Frequently Asked Questions About Vaults Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "questionsvault"
+};
+</script>
+
+<style>
+</style>
