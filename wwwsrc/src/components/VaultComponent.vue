738ff363be0d3b197ef166a5298bb215d8fc32c4
mikedilulo <mikedilulo2019@gmail.com>
2020-03-04 14:02:37 -0700

    Set up Vault View and Vault Component

---
+++ wwwsrc/src/components/VaultComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="vault-component container-fluid">
+    <div class="row">
+      <div class="col-md-2">
+        <div class="card" style="width: 18rem;">
+          <img
+            src="https://images.unsplash.com/photo-1462045504115-6c1d931f07d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p
+              class="card-text"
+            >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vaultComponent",
+  props: ["vaultData"]
+};
+</script>
+
+<style>
+</style>
--- wwwsrc/src/views/Vault.vue
+++ wwwsrc/src/views/Vault.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="vaults container-fluid pr-0 pl-0">
     <homenav />
+    <div class="vault-render container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div v-for="vault in vaults" :key="vault.id">
+            <vaultComponent :vaultData="vault" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import vaultComponent from "@/components/VaultComponent.vue";
 import homenav from "@/components/HomeNavbar.vue";
 export default {
   name: "vaults",
+  mounted() {
+    this.$store.dispatch("getAllVaults");
+  },
+  props: ["vaultData"],
   components: {
-    homenav
+    homenav,
+    vaultComponent
+  },
+  computed: {
+    vaults() {
+      return this.$store.state.vaults;
+    }
   }
 };
 </script>
