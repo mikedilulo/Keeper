91837b40c9a157a8b1db1f95d8232c89fb11a075
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:31:15 -0700

    Information Page on hottest keeps coming soon

---
+++ wwwsrc/src/views/informationHottestKeeps.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="informationhottestkeeps">
+    <h1>Information On Hottest Keeps Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "informationhottestkeeps"
+};
+</script>
+
+<style>
+</style>
