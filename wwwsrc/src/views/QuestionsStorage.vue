694744e7a3b7a50b9bbaca8e3e38d918d392f731
mikedilulo <mikedilulo2019@gmail.com>
2020-03-03 18:33:33 -0700

    Frequently Asked Questions Storage Coming Soon

---
+++ wwwsrc/src/views/QuestionsStorage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="questionsstorage">
+    <h1>Frequently Asked Questions About storage coming soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "questionstorage"
+};
+</script>
+
+<style>
+</style>
