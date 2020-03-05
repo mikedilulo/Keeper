f2305542d27725dcd43f12f1ea273cac7eab81a2
mikedilulo <mikedilulo2019@gmail.com>
2020-03-05 11:49:35 -0700

    SQL setup for Vault Keeps

--- db-setup.sql
+++ db-setup.sql
@@ -28,11 +28,32 @@ USE keeper007;
 --     PRIMARY KEY (id)
 -- );
 
+-- CREATE TABLE vaultkeeps(
+--   id int AUTO_INCREMENT NOT NULL,
+--   userId VARCHAR(255) NOT NULL,
+--   keepId int NOT NULL,
+--   vaultId int NOT NULL,
+--   PRIMARY KEY (id),
+--   INDEX(vaultId, keepId),
+--   INDEX(userId)
+
+--   FOREIGN KEY(vaultId)
+--   REFERENCES vaults(id),
+--   ON DELETE CASCADE,
+
+--   FOREIGN KEY(keepId)
+--   REFERENCES keeps(id),
+--   ON DELETE CASCADE
+-- );
+
 -- SELECT * FROM keeps;
 -- SELECT * FROM vaults;
+-- SELECT * FROM vaultkeeps;
 
 -- TRUNCATE TABLE keeps;
 -- TRUNCATE TABLE vaults;
+-- TRUNCATE TABLE vaultkeeps;
 
 -- DROP TABLE keeps;
 -- DROP TABLE vaults;
+-- DROP TABLE vaultkeeps;
