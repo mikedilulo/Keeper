ff497c9fd2f513ca197fb80c55166283772806ec
mikedilulo <mikedilulo2019@gmail.com>
2020-03-05 11:39:21 -0700

    VK Files shelled out to start building

---
+++ Controllers/VaultKeepsController.cs
@@ -0,0 +1,11 @@
+namespace Keepr.Controllers
+{
+  public class VaultKeepsController : ControllerBase
+  {
+    private readonly VaultKeepsService _vks;
+    public VaultKeepsController(VaultKeepsService vks)
+    {
+      _vks = vks;
+    }
+  }
+}
