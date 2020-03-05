0de48a9ae53f4552ef2f14ca22d5a39edeba5a22
mikedilulo <mikedilulo2019@gmail.com>
2020-03-05 12:11:16 -0700

    VK Repository Completed / Ready for Testing

--- Repositories/VaultKeepsRepository.cs
+++ Repositories/VaultKeepsRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using Dapper;
 using Keepr.Models;
 
 namespace Keepr.Repositories
@@ -15,21 +16,38 @@ namespace Keepr.Repositories
 
     internal IEnumerable<Keep> GetKeepsByVaultId(int vaultId, string userId)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT k.* FROM vaultkeeps vk
+      INNER JOIN keeps k ON k.id = vk.keepId 
+      WHERE (vk.vaultId = @vaultId AND vk.userId = @userId)";
+      return _db.Query<Keep>(sql, new { vaultId, userId });
     }
     internal VaultKeep Find(int keepId, int vaultId)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      SELECT * FROM vaultkeeps
+      WHERE (keepId = @keepId AND vaultId = @vaultId)";
+      return _db.QueryFirstOrDefault<VaultKeep>(sql, new { keepId, vaultId });
     }
 
     internal int CreateVaultKeep(VaultKeep vaultKeepData)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO vaultkeeps
+      (keepId, vaultId, userId)
+      VALUES
+      (@keepId, @vaultId, @userId);
+      SELECT LAST_INSERT_ID();
+      ";
+      int id = _db.ExecuteScalar<int>(sql, vaultKeepData);
+      vaultKeepData.Id = id;
+      return id;
     }
 
     internal void DeleteVaultKeepByVaultId(int keepId, int vaultId, string userId)
     {
-      throw new NotImplementedException();
+      string sql = "DELETE FROM vaultkeeps WHERE keepId = @keepId AND vaultId = @vaultId AND userId = @userId";
+      _db.Execute(sql, new { keepId, vaultId, userId });
     }
 
   }
