177843637de02f876a4fe7b59a60384d4a0f7239
mikedilulo <mikedilulo2019@gmail.com>
2020-03-10 12:53:09 -0600

    Added header to Private Keeps

--- wwwsrc/src/views/PrivateKeep.vue
+++ wwwsrc/src/views/PrivateKeep.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="privatekeep container-fluid pr-0 pl-0">
     <homenav />
+    <h1 class="d-flex justify-content-center mt-3 mb-3">Private Keeps</h1>
     <div v-for="keep in privateKeeps" :key="keep.id">
       <privatekeepcomponent :keepData="keep" />
     </div>
