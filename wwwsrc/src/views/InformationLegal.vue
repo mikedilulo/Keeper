042da15eeede656915dc73c1070f59c7430f51f2
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 13:53:53 -0600

    Added navbar to legal page

--- wwwsrc/src/views/InformationLegal.vue
+++ wwwsrc/src/views/InformationLegal.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="informationlegal container-fluid pr-0 pl-0">
+    <aboutnav />
     <h1>Information About Legal is coming soon</h1>
   </div>
 </template>
 
 <script>
+import aboutnav from "@/components/AboutNavbar.vue";
 export default {
-  name: "informationlegal"
+  name: "informationlegal",
+  components: {
+    aboutnav
+  }
 };
 </script>
 
