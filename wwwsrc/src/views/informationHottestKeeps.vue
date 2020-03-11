6efb7124a063ad31672ae0aa9833a7610a2df794
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 13:54:36 -0600

    Added navbar to hottest keeps page

--- wwwsrc/src/views/informationHottestKeeps.vue
+++ wwwsrc/src/views/informationHottestKeeps.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="informationhottestkeeps container-fluid pr-0 pl-0">
+    <aboutnav />
     <h1>Information On Hottest Keeps Coming Soon</h1>
   </div>
 </template>
 
 <script>
+import aboutnav from "@/components/AboutNavbar.vue";
 export default {
-  name: "informationhottestkeeps"
+  name: "informationhottestkeeps",
+  components: {
+    aboutnav
+  }
 };
 </script>
 
