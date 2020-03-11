66003c6b3bf042e3b9c154bce75a1e765f3af9da
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 15:21:50 -0600

    Added policy card component / started formatting

---
+++ wwwsrc/src/components/PolicyCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="policycard container-fluid">
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p class="card-text">Create Public Keeps you can share with friends, family and users</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Create vaults to store all of your created keeps, and keeps shared from other users that were created.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >View the keep details of who it was created by, what ID the keep is, and how many times the keep has been viewed by other people.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Secure your keeps within vaults. This allows you to keep them safe if they get deleted by the original user</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Subscribe to Keeper to recieve monthly updates, and newsletters. This will include trending keeps, along with other information.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Create Private Keeps that only you the creator can view. Nobody will have access to these keeps but the creator.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "policycard"
+};
+</script>
+
+<style>
+.card {
+  border: none;
+}
+</style>
--- wwwsrc/src/views/InformationPolicy.vue
+++ wwwsrc/src/views/InformationPolicy.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="informationpolicy container-fluid pr-0 pl-0">
     <aboutnav />
-    <h1>Information Policy Page Coming soon</h1>
+    <div class="informationpolicies container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-light d-flex justify-content-center mt-5">Keeper Policy</h1>
+          <p
+            class="text-light d-flex justify-content-center mt-3"
+          >Here at Keeper we frown upon anything that goes against the policy when using this site. Please take the time to read the cards below. If you have any questions, please send an email on the Contact Us page.</p>
+        </div>
+      </div>
+    </div>
+    <div class="middle container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <policycard />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import aboutnav from "@/components/AboutNavbar.vue";
+import policycard from "@/components/InformationCard.vue";
 export default {
   name: "informationpolicy",
   components: {
-    aboutnav
+    aboutnav,
+    policycard
   }
 };
 </script>
 
 <style>
+.informationpolicies {
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://images.unsplash.com/photo-1450101499163-c8848c66ca85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80");
+  background-size: cover;
+  background-repeat: no-repeat;
+  max-width: 100%;
+  height: 100%;
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
 </style>
