28ec0cea0a22e19f8ed13a1f3a76cf5381f04859
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 13:53:05 -0600

    Added navbar to policy page

--- wwwsrc/src/views/InformationPolicy.vue
+++ wwwsrc/src/views/InformationPolicy.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="informationpolicy container-fluid pr-0 pl-0">
+    <aboutnav />
     <h1>Information Policy Page Coming soon</h1>
   </div>
 </template>
 
 <script>
+import aboutnav from "@/components/AboutNavbar.vue";
 export default {
-  name: "informationpolicy"
+  name: "informationpolicy",
+  components: {
+    aboutnav
+  }
 };
 </script>
 
