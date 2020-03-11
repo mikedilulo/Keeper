c4eb5ddc7e70d3b7d19d39ffdffdbe79a4703556
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 17:57:19 -0600

    Added HottestKeepComponent to Components

---
+++ wwwsrc/src/components/HottestKeepCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hottestcard container-fluid">
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Do not steal or acquire the rights to this website. You can be persecuted in the court of law if you do so.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >Hacking is not allowed into this website. If you decide to hack into somebody's account and are reported. We will hunt you down and make sure you pay back the damages.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >This is a safe and secure website for people to connect and have fun. Please do not go out of your way to ruin this experience for anybody. If you do so, you will be fined.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >If you violate the rights of the website, you will be arrested and tried in a court of law to determine your punishment.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >All rights are reserved to the original owner of this website. If you decide to clone or create this website you will be fined. Contact CodeMonkey about details.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center text-danger">Legal:</h1>
+            <strong>
+              <p
+                class="card-text"
+              >It is illegal to make fun of huskies in anyway shape or form on this website and towards others. If so Maya, CodeMonkey's faithful watchdog will sniff you out. Be generous to everybody.</p>
+            </strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hottestcard"
+};
+</script>
+
+<style>
+.card {
+  border: none;
+}
+</style>
