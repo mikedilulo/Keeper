77dbdf069499c10b5d57deceea717c729908ec95
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 14:09:45 -0600

    Added information card component for view

---
+++ wwwsrc/src/components/InformationCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="infocard container-fluid">
+    <div class="row">
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-pager"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Keep FAQ</p>
+            <router-link :to="{name: 'questionskeep'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Keep FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-box"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Vault FAQ</p>
+            <router-link :to="{name: 'questionsvault'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Vault FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-warehouse"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Storage FAQ</p>
+            <router-link :to="{name: 'questionsstorage'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Storage FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-share-square"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Shares FAQ</p>
+            <router-link :to="{name: 'questionsshare'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Shares FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-fire"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Trending FAQ</p>
+            <router-link :to="{name: 'questionstrending'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Trending FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h1 class="card-title d-flex justify-content-center">
+              <i class="fas fa-user-secret"></i>
+            </h1>
+            <p class="card-text">Click the button below to navigate to the Privacy FAQ</p>
+            <router-link :to="{name: 'questionsprivacy'}">
+              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Privacy FAQ</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "infocard"
+};
+</script>
+
+<style>
+.card {
+  border: none;
+}
+</style>
