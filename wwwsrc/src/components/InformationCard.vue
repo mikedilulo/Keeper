b69fe910ef2758913b587ce3c920fe8aed674902
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 14:17:17 -0600

    Fourth infocard edited for users

--- wwwsrc/src/components/InformationCard.vue
+++ wwwsrc/src/components/InformationCard.vue
@@ -32,13 +32,10 @@
       <div class="col-md-4 d-flex justify-content-around mt-5 mb-5">
         <div class="card" style="width: 18rem;">
           <div class="card-body">
-            <h1 class="card-title d-flex justify-content-center">
-              <i class="fas fa-share-square"></i>
-            </h1>
-            <p class="card-text">Click the button below to navigate to the Shares FAQ</p>
-            <router-link :to="{name: 'questionsshare'}">
-              <a href="#" class="btn btn-dark d-flex justify-content-center">Go To Shares FAQ</a>
-            </router-link>
+            <h1 class="card-title d-flex justify-content-center">Users Can:</h1>
+            <p
+              class="card-text"
+            >Secure their keeps within vaults. This allows you to keep them safe if they get deleted by the original user</p>
           </div>
         </div>
       </div>
