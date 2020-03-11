7660c32176d80b2baceaceef470944276c5b3349
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 15:26:15 -0600

    Second Policy Card completed / adhere to Mobile

--- wwwsrc/src/components/PolicyCard.vue
+++ wwwsrc/src/components/PolicyCard.vue
@@ -16,11 +16,11 @@
       <div class="col-md-4 d-flex justify-content-around mb-5 mt-5">
         <div class="card" style="width: 18rem;">
           <div class="card-body">
-            <h1 class="card-title d-flex justify-content-center text-danger">Users Can:</h1>
+            <h1 class="card-title d-flex justify-content-center text-danger">User Policy:</h1>
             <strong>
               <p
                 class="card-text"
-              >Create vaults to store all of your created keeps, and keeps shared from other users that were created.</p>
+              >Keeper is a place to connect with other users through mutual humor of keeps you can build. Please do not be vulgar or offensive with different users.</p>
             </strong>
           </div>
         </div>
