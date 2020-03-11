b90b4963fb23ad2b98ab127f6a2eee97454dfc91
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 13:49:52 -0600

    Added navbar to users page

--- wwwsrc/src/views/InformationUsers.vue
+++ wwwsrc/src/views/InformationUsers.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="informationuser">
+  <div class="informationuser container-fluid pr-0 pl-0">
+    <aboutnav />
     <h1>Information About Users Coming Soon</h1>
   </div>
 </template>
 
 <script>
+import aboutnav from "@/components/AboutNavbar.vue";
 export default {
-  name: "informationuser"
+  name: "informationuser",
+  components: {
+    aboutnav
+  }
 };
 </script>
 
