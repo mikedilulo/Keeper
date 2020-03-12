3a104415bc56a58f3671d70a5da65b0ada921b0a
mikedilulo <mikedilulo2019@gmail.com>
2020-03-11 18:00:42 -0600

    Adjusted background for hottest keeps

--- wwwsrc/src/views/informationHottestKeeps.vue
+++ wwwsrc/src/views/informationHottestKeeps.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="informationhottestkeeps container-fluid pr-0 pl-0">
     <aboutnav />
-    <h1>Information On Hottest Keeps Coming Soon</h1>
+    <div class="informationhot container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-light d-flex justify-content-center mt-5">Keeper Hottest Keeps</h1>
+          <p
+            class="text-light d-flex justify-content-center mt-3"
+          >Please review the legalities of Keeper below. If any issues arise or are a concern, please reach out to CodeMonkey via the contact page.</p>
+        </div>
+      </div>
+    </div>
+    <div class="middle container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <legalcard />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import aboutnav from "@/components/AboutNavbar.vue";
+import legalcard from "@/components/LegalCard.vue";
 export default {
   name: "informationhottestkeeps",
   components: {
-    aboutnav
+    aboutnav,
+    legalcard
   }
 };
 </script>
 
 <style>
+.informationhot {
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("https://images.unsplash.com/photo-1556960653-9f195e88c52e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80");
+  background-size: cover;
+  background-repeat: no-repeat;
+  max-width: 100%;
+  height: 100%;
+  padding-top: 80px;
+  padding-bottom: 80px;
+}
 </style>
